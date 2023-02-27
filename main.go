2514a3d2332f6e68d3f8cad0a8575714b6e35fa7
MartiFiak <jacolotmarine@gmail.com>
2023-02-27 15:54:36 +0100

    [Front]Index

--- main.go
+++ main.go
@@ -12,7 +12,6 @@ func main() {
 	http.Handle("/templates/", http.StripPrefix("/templates/", http.FileServer(http.Dir("./templates"))))
 	http.Handle("/dist/", http.StripPrefix("/dist/", http.FileServer(http.Dir("./dist"))))
 	http.HandleFunc("/", page.Index)
-	http.HandleFunc("/login", page.LoginFunc)
 	port := "8080"
 	fmt.Println("Startup Server on port " + port)
 	err := http.ListenAndServe(":"+port, nil)
