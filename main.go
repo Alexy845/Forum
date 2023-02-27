dd2119ff2378406fbc0834cace61a66840c8ae6b
gkillian <killian.guillemot@ynov.com>
2023-02-27 09:53:03 +0100

    Add File

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
 package forum
+
+import (
+	"fmt"
+	page "forum/page"
+	"net/http"
+)
+
+func main() {
+	http.Handle("/templates/", http.StripPrefix("/templates/", http.FileServer(http.Dir("./templates"))))
+	http.Handle("/dist/", http.StripPrefix("/dist/", http.FileServer(http.Dir("./dist"))))
+	http.HandleFunc("/", page.Index)
+	port := "80"
+	fmt.Println("Startup Server on port " + port)
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		return
+	}
+}
---
+++ page/index.go
@@ -0,0 +1,11 @@
+package page
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("templates/index.html")
+	t.Execute(w, nil)
+}
