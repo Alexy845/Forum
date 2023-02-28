29fd2b81e1494fedd27a699bec3904034c57c720
Alexy845 <alexy.daubresse@ynov.com>
2023-02-28 09:41:51 +0100

    test

--- go.mod
+++ go.mod
@@ -4,4 +4,4 @@ go 1.19
 
 require golang.org/x/crypto v0.6.0
 
-require github.com/mattn/go-sqlite3 v1.14.16 // indirect
+
--- page/login.go
+++ page/login.go
@@ -8,30 +8,30 @@ import (
 func LoginFunc(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("./templates/html/login.html"))
 
-	switch r.Method {
-	case "GET":
-	case "POST":
-		username := r.FormValue("username")
-		password := r.FormValue("password")
-		if password != "" {
-			if username != ""{
-				if GetUserData(username).Username != "" && GetUserData(username).Password == password {
-					CurrentUser = GetUserData(username)
-				}
-			}
-		}
-	}
+	// switch r.Method {
+	// case "GET":
+	// case "POST":
+	// 	username := r.FormValue("username")
+	// 	password := r.FormValue("password")
+	// 	if password != "" {
+	// 		if username != ""{
+	// 			if GetUserData(username).Username != "" && GetUserData(username).Password == password {
+	// 				CurrentUser = GetUserData(username)
+	// 			}
+	// 		}
+	// 	}
+	// }
 	tmpl.Execute(w, nil)
 }
 
-func GetUserData(username string) User{
-	users := _GetUsers()
+// func GetUserData(username string) User{
+// 	users := _GetUsers()
 
-	for i, user := range users {
-		if user.Username == username {	
-			return users[i]
-		}
-	}
-	return User{Username:""}
-}
+// 	for i, user := range users {
+// 		if user.Username == username {	
+// 			return users[i]
+// 		}
+// 	}
+// 	return User{Username:""}
+// }
 
